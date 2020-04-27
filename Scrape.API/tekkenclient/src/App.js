c10e448546d9e232dca2643ea945158c01f098e9
Grant Horner <h.grant.horner@gmail.com>
2020-04-27 13:35:00 -0400

    Cleaned up fsproj

--- Scrape.API/tekkenclient/src/App.js
+++ Scrape.API/tekkenclient/src/App.js
@@ -1,43 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        fetch("https://localhost:5001/api/characters")
-            .then(d => d.json())
-            .then(setData)
-    }, [])
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <section>
-                <ul>
-                    {
-                        Array.isArray(data) ? data.map((d, i) => (
-                                <li key={i}>{d}</li>
-                            ))
-                            : data
-                    }
-                </ul>
-            </section>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
